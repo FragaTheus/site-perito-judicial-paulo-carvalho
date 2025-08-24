c4a23eb612222e42dbdcffd633a4e015a8186018
Matheus Augusto <matheusguto1@hotmail.com>
2025-08-24 12:41:08 -0300

    criando a pg-servicos

---
+++ src/pages/servicos/Servicos.tsx
@@ -0,0 +1,17 @@
+import { HeroSection } from "../../components/hero/HeroSection";
+import { PageLayout } from "../../layout/page-layout/PageLayout";
+
+export const PageServicos = () => {
+  return (
+    <PageLayout
+      heroComponent={
+        <HeroSection
+          imgUrl="hero-home.jpg"
+          title="Áreas de atuação"
+          description="Engenheiro de segurança do trabalho | Perito Judicial"
+        ></HeroSection>
+      }
+      sections={[]}
+    ></PageLayout>
+  );
+};
