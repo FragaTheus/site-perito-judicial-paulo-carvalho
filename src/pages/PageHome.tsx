82a63011dadbb2adbc4aa1ced66f03db6f1e9b21
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2025-12-15 11:13:39 -0300

    refac: Tirando sessao cases

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -1,7 +1,6 @@
 import { FAQ } from "../sections/home-page/FAQ";
 import { Atuacao } from "../sections/home-page/Atuacao";
 import { Beneficios } from "../sections/home-page/Beneficios";
-import { Cases } from "../sections/home-page/Cases";
 import { CTA } from "../sections/home-page/Cta";
 import { CTAMid } from "../sections/home-page/CTAMid";
 import { HomeHero } from "../sections/home-page/HomeHero";
@@ -16,7 +15,6 @@ export const PageHome = () => {
       <IntroHome />
       <Beneficios />
       <Atuacao />
-      <Cases />
       <CTAMid />
       <Processo />
       <Sobre />
